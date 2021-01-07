a230edc588741b635aeab20c7e0064232a7d288a
Valentyn Bochkarov <bochkarov90@gmail.com>
2021-01-07 21:34:30 +0100

    Finishing touches: didFinishLaunchingWithOptions

--- HWS2021 Project 7 - Whitehouse Petitions/ViewController.swift
+++ HWS2021 Project 7 - Whitehouse Petitions/ViewController.swift
@@ -14,13 +14,23 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+    
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                return
             }
         }
+        
+        showError()
     }
     func parse(json: Data) {
         let decoder = JSONDecoder()
@@ -47,6 +57,12 @@ class ViewController: UITableViewController {
         let vc = DetailViewController()
         vc.detailItem = petitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
 }
 
